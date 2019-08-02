5422cff19a63f67534d1edf2f0fc6ef0507fe460
Gabriel Mello <gabrielmello757@gmail.com>
2019-08-02 14:44:46 -0300

    alteração para victor

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => (
   <Provider store={store}>
     <Helmet>
       <meta charSet="utf-8" />
-      <title>Gabriel</title>
+      <title>Victor</title>
       <meta
         name="description"
         content="Save interesting tools in a safe place. From programmers to programmers."
